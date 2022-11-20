7bd379a3f6b2a9031b4e2668912bddb7022ed424
unknown <castanedajustine09@gmail.com>
2022-11-20 13:54:42 +0800

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require("http");
 const { Server } = require("socket.io");
-const { isPromise } = require('util/types');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
